c385249425a3012f9d2ab776fde8d37ad67451c8
xaviermatos <xavierintheam@gmail.com>
2022-05-18 21:09:35 -0400

    Added the website
    My professional resume website.

---
+++ XMatosOnlineResume/myScript.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+	$(".dropdown").on("hide.bs.dropdown", function () {
+		$(".click").html('Resume <span class="caret"></span>');
+	});
+	$(".dropdown").on("show.bs.dropdown", function () {
+		$(".click").html('Resume <span class="caret caret-up"></span>');
+	});
+});
+//Flips the dropdown carrot when the dropdown link is clicked.
+
+$(document).scroll(function () {
+	if ($(this).scrollTop() > a) {
+		$('.navbar').css({
+			"background": "Black"
+		});
+	} else {
+		$('.navbar').css({
+			"background": "rgba(10, 10, 10, 0.6)"
+		});
+	}
+});
+//flips the carrot back to normal when the dropdown link is clicked again.
+
+$(window).scroll({
+		previousTop: 0
+	},
+	function () {
+		var currentTop = $(window).scrollTop();
+		if (currentTop < this.previousTop) {
+			$(".navbar").fadeIn();
+
+		} else {
+			$(".navbar").fadeOut();
+		}
+		this.previousTop = currentTop;
+	});
+
+var a = $(".navbar");
+//Removes the navbar when scrolling down and returns it when scrolling up.
