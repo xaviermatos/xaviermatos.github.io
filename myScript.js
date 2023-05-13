c812ccbc64d09d42aa8d8bd9ef24133c5b17c940
xaviermatos <xavierintheam@gmail.com>
2023-05-13 17:39:37 -0400

    Update myScript.js
    Added a JavaScript fix to the mobile anchor links issue.

--- myScript.js
+++ myScript.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
 	$(".dropdown").on("hide.bs.dropdown", function () {
-		$(".click").html('Resume <span class="caret"></span>');
-	});
+			$(".click").html('Resume <span class="caret"></span>');
+		});
+
 	$(".dropdown").on("show.bs.dropdown", function () {
-		$(".click").html('Resume <span class="caret caret-up"></span>');
-	});
+			$(".click").html('Resume <span class="caret caret-up"></span>');
+		});
 });
 //Flips the dropdown carrot when the dropdown link is clicked.
 
@@ -13,27 +14,91 @@ $(document).scroll(function () {
 		$('.navbar').css({
 			"background": "Black"
 		});
-	} else {
-		$('.navbar').css({
-			"background": "rgba(10, 10, 10, 0.6)"
-		});
-	}
+}
+
+else {
+	$('.navbar').css({
+		"background": "rgba(10, 10, 10, 0.6)"
+	});
+}
 });
 //flips the carrot back to normal when the dropdown link is clicked again.
 
 $(window).scroll({
-		previousTop: 0
-	},
-	function () {
-		var currentTop = $(window).scrollTop();
-		if (currentTop < this.previousTop) {
-			$(".navbar").fadeIn();
+previousTop: 0
+}
 
-		} else {
-			$(".navbar").fadeOut();
-		}
-		this.previousTop = currentTop;
-	});
+,
+function () {
+var currentTop=$(window).scrollTop();
 
-var a = $(".navbar");
+if (currentTop < this.previousTop) {
+	$(".navbar").fadeIn();
+
+}
+
+else {
+	$(".navbar").fadeOut();
+}
+
+this.previousTop=currentTop;
+});
+
+var a=$(".navbar");
 //Removes the navbar when scrolling down and returns it when scrolling up.
+
+//Allows for the anchor links to work on mobile
+//This link expains why anchor links don't work the same on mobile and how to fix it: https://codepen.io/2kool2/pen/JjYGdmZ
+const fixInPageAnchors=(_=> {
+	return;
+	'use strict'
+	const currentURL=self.location.href;
+
+	const scrollAndFocus=target=> {
+		target.setAttribute('tabindex', '-1');
+
+		target.addEventListener('blur', _=> target.removeAttribute('tabindex'), {
+			once: true
+		});
+
+	requestAnimationFrame(_=> {
+			target.scrollIntoView();
+
+			target.focus({
+				preventScroll:true
+			});
+	});
+}
+
+;
+
+const anchorClicked=e=> {
+const a=e.target;
+const toID=a.hash.split('#')[1];
+const toObj=document.getElementById(toID);
+
+//If the link is invalid return the default behaviour.
+if ( !toObj) return;
+
+e.preventDefault();
+
+//Updating the URL fragment identifier to allow the back button to work
+window.location.hash=toID;
+
+scrollAndFocus(toObj);
+}
+
+;
+
+const pageAnchors=document.getElementsByTagName('a');
+
+for (const a of pageAnchors) {
+
+//Filter for the in-page anchors
+if ( !a.hash) continue;
+if (a.origin + a.pathname !==self.location.origin + self.location.pathname) continue;
+
+a.addEventListener('click', anchorClicked);
+}
+
+})();
