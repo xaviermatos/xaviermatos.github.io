62049217f19cbcd4d495a28da8db4aaed2ae9275
xaviermatos <xavierintheam@gmail.com>
2023-05-15 20:25:26 -0400

    Update myScript.js
    Added the removal of the navbar when a link is clicked, fixed the order of the comments, and beautified the code.

--- myScript.js
+++ myScript.js
@@ -1,55 +1,51 @@
-$(document).ready(function () {
-	$(".dropdown").on("hide.bs.dropdown", function () {
-			$(".click").html('Resume <span class="caret"></span>');
-		});
-
-	$(".dropdown").on("show.bs.dropdown", function () {
-			$(".click").html('Resume <span class="caret caret-up"></span>');
-		});
-});
 //Flips the dropdown carrot when the dropdown link is clicked.
+$(document).ready(function() {
+	$(".dropdown").on("hide.bs.dropdown", function() {
+		$(".click").html('Resume <span class="caret"></span>');
+	});
 
-$(document).scroll(function () {
+	$(".dropdown").on("show.bs.dropdown", function() {
+		$(".click").html('Resume <span class="caret caret-up"></span>');
+	});
+});
+
+//flips the carrot back to normal when the dropdown link is clicked again.
+$(document).scroll(function() {
 	if ($(this).scrollTop() > a) {
 		$('.navbar').css({
 			"background": "Black"
 		});
-}
-
-else {
-	$('.navbar').css({
-		"background": "rgba(10, 10, 10, 0.6)"
-	});
-}
-});
-//flips the carrot back to normal when the dropdown link is clicked again.
-
-$(window).scroll({
-previousTop: 0
-}
-
-,
-function () {
-var currentTop=$(window).scrollTop();
-
-if (currentTop < this.previousTop) {
-	$(".navbar").fadeIn();
-
-}
-
-else {
-	$(".navbar").fadeOut();
-}
-
-this.previousTop=currentTop;
+	} else {
+		$('.navbar').css({
+			"background": "rgba(10, 10, 10, 0.6)"
+		});
+	}
 });
 
-var a=$(".navbar");
 //Removes the navbar when scrolling down and returns it when scrolling up.
+$(window).scroll({
+		previousTop: 0
+	}
 
-//Attempt to fix mobile anchors
- $('a.native-anchor').bind('click', function(ev) {
-         var target = $( $(this).attr('href') ).get(0).offsetTop;
-         $.mobile.silentScroll(target);
-         return false;
-     });
+	,
+	function() {
+		var currentTop = $(window).scrollTop();
+
+		if (currentTop < this.previousTop) {
+			$(".navbar").fadeIn();
+
+		} else {
+			$(".navbar").fadeOut();
+		}
+
+		this.previousTop = currentTop;
+	});
+
+var a = $(".navbar");
+
+//Hides the navbar whenever any navigation menu link is clicked.
+$(document).ready(function() {
+	$(".hide-nav").click(function() {
+		$(".navbar-default").hide();
+	});
+});
